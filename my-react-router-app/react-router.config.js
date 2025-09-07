5470cd8daafe021ff601268af3e3e72fc68a6cf2
Sabin <sabinanwar6@gmail.com>
2025-09-07 16:28:14 +0200

    Add initial project files and configuration for React Router with Vercel support

--- my-react-router-app/react-router.config.js
+++ my-react-router-app/react-router.config.js
@@ -3,7 +3,9 @@ import { vercelPreset } from "@vercel/react-router/vite";
 
 /** @type {import('@react-router/dev/config').Config} */
 export default {
-  // SSR ist hier richtig, weil du kein index.html hast
   ssr: true,
   presets: [vercelPreset()],
+  ssrBuild: {
+    outDir: "build/server", // <--- force static output
+  },
 };
--- my-react-router-app/vite.config.js
+++ my-react-router-app/vite.config.js
@@ -1,6 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/react-router/vite";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
+  ssr: true,
+  presets: [vercelPreset()],
   plugins: [reactRouter()],
+  ssrBuild: {
+    outDir: "build/server", // <--- force static output
+  },
 });
